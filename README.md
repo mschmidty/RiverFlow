2d496ef62173946688e3cbb9b5f4ff9de461c6c7
mschmidty <mschmidty@gmail.com>
2018-06-29 08:59:28 -0600

    first commit, start at doloresRiver2

---
+++ README.md
@@ -0,0 +1,8 @@
+# An App that Automatically Updates River Flow data
+Created with [p5.js](https://p5js.org/) and a little bit of jQuery.
+
+## Feature todo
+* Query both the upper and lower Dolores Reaches
+* Create dropdown from a .json file so that user can select River (DONE).
+* Pass the selected value to the loadXML function to get the data.  
+* Only display the data once the dropdown has been selected. 
---
+++ sketch.js
@@ -0,0 +1,53 @@
+var hucData=[];
+
+//To Find discharge go here look for this value: "wml2:value".
+function setup() {
+	noCanvas();
+	// Set up Dropdown lists.
+	hucData = loadJSON('./data/huc14.json');
+	let dropdown = $('#hucCode');
+	dropdown.empty;
+
+	dropdown.append('<option selected="true" disabled>Choose Hydrologic Unit</option>');
+	dropdown.prop('selectedIndex',0);
+
+	const url = './data/huc14.json';
+
+	$.getJSON(url,function(data){
+		$.each(data, function(key,entry){
+			dropdown.append($('<option></option>').attr('value', entry.HUC).text(entry.Name));
+		})
+	});
+
+	//Load data from USGS water services API.
+	loadXML('https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&huc=14030002&parameterCd=00060,00065&siteStatus=all', loadData)
+
+}
+
+
+
+function loadData(data){
+	console.log(data);
+	//Loop through the data
+	var stations = data.children;
+	for(var i = 0; i<stations.length; i++){
+		var locationAll = stations[i].children[0].children[1].content.split("at ")[1];
+		var flow = stations[i].children[0].children[3].children[0].children[5].children[0].children[1].children[0].children[1].content;
+		var river;
+		var location;
+		if(locationAll.indexOf('AT')> -1){
+			river = locationAll.split("AT ")[0];
+			location = 'At ' + locationAll.split("AT ")[1];
+		}else if(locationAll.indexOf('NEAR')> -1){
+			river = locationAll.split("NEAR ")[0];
+			location = 'Near ' + locationAll.split("NEAR ")[1];
+		}else{
+			river = locationAll.split("BELOW ")[0];
+			location = 'Below ' + locationAll.split("BELOW ")[1];
+		}
+
+		$('tbody').append("<tr>" + "<td>" + river + "</td>" + "<td>" + flow +  "</td>" + "<td>" + location + "</td>" + "</tr>");
+
+	}
+
+}
