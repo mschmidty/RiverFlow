ad485a21b125e05528568bfe42a0dfb357bfb81a
mschmidty <mschmidty@gmail.com>
2018-06-29 10:13:12 -0600

    added dropdown updating

--- sketch.js
+++ sketch.js
@@ -3,31 +3,40 @@ var hucData=[];
 //To Find discharge go here look for this value: "wml2:value".
 function setup() {
 	noCanvas();
-	// Set up Dropdown lists.
+	// Read data/huc14.json to create Dropdown lists.
 	hucData = loadJSON('./data/huc14.json');
 	let dropdown = $('#hucCode');
 	dropdown.empty;
-
 	dropdown.append('<option selected="true" disabled>Choose Hydrologic Unit</option>');
 	dropdown.prop('selectedIndex',0);
-
 	const url = './data/huc14.json';
-
 	$.getJSON(url,function(data){
 		$.each(data, function(key,entry){
 			dropdown.append($('<option></option>').attr('value', entry.HUC).text(entry.Name));
 		})
 	});
 
-	//Load data from USGS water services API.
-	loadXML('https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&huc=14030002&parameterCd=00060,00065&siteStatus=all', loadData)
 
+
+	//Load data from USGS water services API.
+	$('#hucCode').change(function(){
+		var hucValue="";
+		$('#hucCode option:selected').each(function(){
+			hucValue += $(this).val()+"";
+			console.log(hucValue);
+			loadXML('https://waterservices.usgs.gov/nwis/iv/?format=waterml,2.0&huc='+hucValue+'&parameterCd=00060,00065&siteStatus=all', loadData);
+
+		})
+	});
 }
 
 
 
 function loadData(data){
 	console.log(data);
+	$('.appendHucData').each(function(){
+		$(this).remove();
+	})
 	//Loop through the data
 	var stations = data.children;
 	for(var i = 0; i<stations.length; i++){
@@ -35,10 +44,10 @@ function loadData(data){
 		var flow = stations[i].children[0].children[3].children[0].children[5].children[0].children[1].children[0].children[1].content;
 		var river;
 		var location;
-		if(locationAll.indexOf('AT')> -1){
+		if(locationAll.indexOf('AT ')> -1){
 			river = locationAll.split("AT ")[0];
 			location = 'At ' + locationAll.split("AT ")[1];
-		}else if(locationAll.indexOf('NEAR')> -1){
+		}else if(locationAll.indexOf('NEAR ')> -1){
 			river = locationAll.split("NEAR ")[0];
 			location = 'Near ' + locationAll.split("NEAR ")[1];
 		}else{
@@ -46,7 +55,7 @@ function loadData(data){
 			location = 'Below ' + locationAll.split("BELOW ")[1];
 		}
 
-		$('tbody').append("<tr>" + "<td>" + river + "</td>" + "<td>" + flow +  "</td>" + "<td>" + location + "</td>" + "</tr>");
+		$('tbody').append("<tr class='appendHucData'>" + "<td>" + river + "</td>" + "<td>" + flow +  "</td>" + "<td>" + location + "</td>" + "</tr>");
 
 	}
 
